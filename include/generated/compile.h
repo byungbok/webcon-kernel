6e001c25b5115107d43f24770b03ce16a98a125e
byungbok <erunsee@falinux.com>
2015-08-22 23:16:30 +0900

    kernel

---
+++ include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 20 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#20 Fri Aug 21 17:25:35 KST 2015"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "lbb-ubuntu"
+#define LINUX_COMPILER "gcc version 4.6.3 20120201 (prerelease) (1.14.1 - nexell) "
---
+++ make_nxp2120.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo make ARCH=arm  CROSS_COMPILE=/opt/crosstools/arm-eabi-4.6.3-glibc-2.13/bin/arm-generic-linux-gnueabi- uImage
+
